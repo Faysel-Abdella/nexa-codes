f41d2d12fa60714c5dee539b1e6f010b557a35ba
Faysel Abdella <fayselcode@gmial.com>
2024-02-18 20:21:49 +0300

    Fix window screen size issue

--- src/app/sections/07_testimonials.tsx
+++ src/app/sections/07_testimonials.tsx
@@ -1,26 +1,36 @@
 "use client"
 
-import { useEffect, useState } from "react";
-import { userTestimonial } from "../data/userTestimonials";
+import { useEffect, useState } from 'react';
+import { userTestimonial } from '../data/userTestimonials';
 import Testimonial from '../components/Testimonial';
 import Marquee from 'react-fast-marquee';
 
 export default function Testimonials() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowWidth, setWindowWidth] = useState(0);
 
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
 
-    window.addEventListener('resize', handleResize);
+    if (typeof window !== 'undefined') {
+      setWindowWidth(window.innerWidth);
+      window.addEventListener('resize', handleResize);
+    }
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      if (typeof window !== 'undefined') {
+        window.removeEventListener('resize', handleResize);
+      }
     };
   }, []);
 
   const gradientWidth = windowWidth > 900 ? 600 : 100;
+
+  if (typeof window === 'undefined') {
+    return null; // Return null or a loading indicator if running in a non-browser environment
+  }
+
   return (
     <div className='my-[15rem]'>
       <Marquee direction='left' gradient={true} gradientWidth={gradientWidth} speed={20}>
