9b7c6b315f71146ea9149d5ba1a1ff066ae3c7eb
Faysel Abdella <fayselcode@gmial.com>
2024-02-20 00:55:30 +0300

    responsive profile with posts

--- src/app/components/BlogMain.tsx
+++ src/app/components/BlogMain.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 const BlogPost = ({ id, header, description, image, date }: any) => {
   return (
-    <div className='my-16 max-small:my-12'>
-      <p className='text-gray-300 text-[16px] leading-5 mb-6'>{date}</p>
-      <div className='flex gap-6 content-center pb-16 border-b-0.5 border-light-border
-      max-small:pb-10
+    <div className='my-16 max-small:my-6'>
+      <p className='text-gray-300 text-[16px] leading-5 mb-6 max-small:text-[12px] max-small:mb-2'>{date}</p>
+      <div className='flex gap-6 content-center pb-16 small:border-b-0.5 small:border-light-border
+      max-small:pb-3
       max-small:gap-4
       '>
         <Image
@@ -19,8 +19,8 @@ const BlogPost = ({ id, header, description, image, date }: any) => {
         <div>
           <div className=' text-[26px] leading-10 text-black font-extrabold  mb-2
           max-midSmall:text-[20px]
-          max-midSmall:font-bold
-          max-small:text-[16px]
+          max-midSmall:font-semibold
+          max-small:text-[13px]
           max-small:leading-5
           '>
             {header}
@@ -28,10 +28,17 @@ const BlogPost = ({ id, header, description, image, date }: any) => {
           <div className='text-[16px] text-black leading-5 font-normal
           max-midSmall:text-[14px]
           max-small:text-[13px]
-          max-small:self-start
+          max-small:hidden
           '>{description}</div>
         </div>
+        
       </div>
+      <div className='small:hidden text-[16px] text-black leading-5 font-normal
+          max-midSmall:text-[14px]
+          max-small:text-[13px]
+          border-b-0.5 border-light-border
+          pb-10
+          '>{description}</div>
     </div>
   );
 }
