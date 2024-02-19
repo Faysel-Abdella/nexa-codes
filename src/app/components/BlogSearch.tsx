a0528b0f5935285f4635f1e45f7ad44385421520
Faysel Abdella <fayselcode@gmial.com>
2024-02-19 09:08:31 +0300

    Fix the search bar issue

--- src/app/components/BlogSearch.tsx
+++ src/app/components/BlogSearch.tsx
@@ -2,58 +2,80 @@ import React from 'react';
 
 import Image from 'next/image';
 import icon from "../assets/search_icon.svg"
+import unfilled_icon from "../assets/unfilled_arrow.svg"
 
 const BlogSearch = () => {
   return (
-    <div className='px-36 mx-auto mt-[15rem]'>
-      <div className='flex items-center space-x-4'>
-        
-          <div className='relative flex items-center w-full bg-white border-gray-300'>
-            <input
-              type='text'
-              id='header-search'
-              placeholder='제목 & 키워드를 검색해 주세요'
-              name='s'
-              className='bg-white border-gray-300 focus:outline-none  w-full py-3 px-10 rounded-full pr-16 '
-            />
-            <div className='absolute right-4'>
-              <div>
-                <Image  src={icon} alt='search-icon'/>
-              </div>
+    <div className='px-36 mx-auto mt-[15rem] 
+    max-medium:px-10
+    max-medium:mt-[10rem]
+    max-small:mt-[7rem]
+    max-small:px-[10px]
+    '>
+      <div className='flex items-center space-x-4
+      max-midSmall:flex-col-reverse
+      max-midSmall:gap-5
+      '>
+
+        <div className='relative flex items-center w-full bg-white border-gray-300'>
+          <input
+            type='text'
+            id='header-search'
+            placeholder='제목 & 키워드를 검색해 주세요'
+            className='bg-white text-black border border-[#ACACAC] focus:outline-none  w-full py-3 px-10 rounded-full pr-16 
+            max-midSmall:pl-5
+            max-midSmall:pr-12
+            max-midSmall:placeholder:text-[13px]
+            '
+          />
+          <div className='absolute right-4 '>
+            <div className='cursor-pointer '>
+              <Image src={icon} alt='search-icon' className='w-[29px] h-[30px]' />
             </div>
           </div>
-
-
-        <div className='flex gap-2 items-center flex-shrink-0 bg-white  px-8 py-4 rounded-full border border-gray-300'>
-          <button type='submit' className='text-gray-700'>
-            최신순
-          </button>
-          <i className='fa-solid fa-angle-down text-gray-700 cursor-pointer'></i>
         </div>
+
+        <div className='max-midSmall:flex max-midSmall:flex-col max-midSmall:items-center max-midSmall:justify-center'>
+          <div className='flex gap-2 items-center flex-shrink-0 bg-white  px-6 py-3 rounded-full border border-gray-300'>
+            <button type='submit' className='text-nowrap text-gray-700 text-[15px]'>
+              최신순
+            </button>
+            <Image src={unfilled_icon} alt='Down arrow' className='w-[19px] h-[13px]' />
+          </div>
+
+          <p className='text-[15px] mt-2 text-veryLight-gray
+           midSmall:hidden
+          '>인기순</p>
+        </div>
+
       </div>
 
 
       <div className='flex justify-between mb-[4rem]'>
-        <div className='flex items-center gap-2 mt-2'>
-          <div className='border border-light-border rounded-full py-1 px-4 text-light-gray text-small-size'>
+        <div className='flex flex-wrap flex-row gap-2 mt-2'>
+          <div className='flex  justify-center items-center border border-light-border rounded-full py-1 px-1 w-[50px] text-light-gray text-[14px]
+          '>
             #AI
           </div>
 
-          <div className='border border-light-border rounded-full py-1 px-4 text-light-gray text-small-size'>
+          <div className='flex justify-center items-center border border-light-border rounded-full py-1 px-1 w-[90px] text-light-gray text-[14px]'>
             #BACKEND
           </div>
-          <div className='border border-light-border rounded-full py-1 px-4 text-light-gray text-small-size'>
+          <div className='flex justify-center items-center border border-light-border rounded-full py-1 px-1 w-[60px] text-light-gray text-[14px]'>
             #AWS
           </div>
-          <div className='border border-light-border rounded-full py-1 px-4 text-light-gray text-small-size'>
+
+          <div className='flex justify-center items-center border border-light-border rounded-full py-1 px-1 w-[90px] text-light-gray text-[14px]'>
             #CULTURE
           </div>
-          <div className='border border-light-border rounded-full py-1 px-4 text-light-gray text-small-size'>
+          <div className='flex justify-center items-center border border-light-border rounded-full py-1 px-1 w-[68px] text-light-gray text-[14px]'>
             #DATA
           </div>
         </div>
 
-        <p className='mt-2 text-veryLight-gray text-lg mr-8   '>인기순</p>
+        <p className='text-[18px] mt-2 text-veryLight-gray  mr-8 
+        max-midSmall:hidden
+        '>인기순</p>
       </div>
     </div>
   );
